c5a794ec5f32192274932bf2ae690eb33b0e9e19
Oleksii Kondratiuk <okondratiuk@sdl.com>
2017-05-21 00:21:33 +0300

    Initial commit. Created Maven project. Started to create infrastructure for Spring boot console application.

---
+++ src/main/java/com/ifelsecoders/ConsoleApplication.java
@@ -0,0 +1,12 @@
+package com.ifelsecoders;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsoleApplication implements CommandLineRunner {
+    @Override
+    public void run(String... strings) throws Exception {
+
+    }
+}
